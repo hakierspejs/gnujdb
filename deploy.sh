73bbfbdaabaad62782da09adfefc549b94cbd2a1
Jacek Wielemborek <d33tah@gmail.com>
2023-12-07 21:09:29 +0100

    Dodaj obsluge dla deploy.sh bez ssh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-rsync -ra --progress --exclude ".git" --exclude "db.sqlite3" --exclude "deploy.sh" --exclude "media/" ./* root@hs-ldz.pl:/home/d33tah/workspace/hs/gnujdb/
-ssh root@hs-ldz.pl "chown -R d33tah:d33tah /home/d33tah/workspace/hs/gnujdb"
-ssh root@hs-ldz.pl "cd /home/d33tah/workspace/hs/gnujdb && docker-compose build"
-ssh root@hs-ldz.pl "cd /home/d33tah/workspace/hs/gnujdb && docker-compose up -d"
-
+if [ "`hostname`" == "hsldz" ]; then
+    chown -R d33tah:d33tah /home/d33tah/workspace/hs/gnujdb
+    cd /home/d33tah/workspace/hs/gnujdb && docker-compose build
+    cd /home/d33tah/workspace/hs/gnujdb && docker-compose up -d
+else
+    rsync -ra --progress --exclude ".git" --exclude "db.sqlite3" --exclude "deploy.sh" --exclude "media/" ./* root@hs-ldz.pl:/home/d33tah/workspace/hs/gnujdb/
+    ssh root@hs-ldz.pl "chown -R d33tah:d33tah /home/d33tah/workspace/hs/gnujdb"
+    ssh root@hs-ldz.pl "cd /home/d33tah/workspace/hs/gnujdb && docker-compose build"
+    ssh root@hs-ldz.pl "cd /home/d33tah/workspace/hs/gnujdb && docker-compose up -d"
+fi
